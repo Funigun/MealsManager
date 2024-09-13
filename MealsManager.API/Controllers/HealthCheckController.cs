f4e5b2d6bc28f83a8edbda31db2427fe0ee73dc8
Patryk Stępnik <stepnikpa@gmail.com>
2024-09-13 22:41:52 +0200

    Added HealthCheck controller

---
+++ MealsManager.API/Controllers/HealthCheckController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MealsManager.API.Controllers
+{
+    /// <summary>
+    /// Health Check controller
+    /// </summary>
+    [Route("api/health-checks")]
+    public class HealthCheckController : ControllerBase
+    {
+        /// <summary>
+        /// Basic Health Check to ensure proper API configuration
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<string>> GetHealthCheckAsync()
+        {
+            return "Healthy";
+        }
+    }
+}
