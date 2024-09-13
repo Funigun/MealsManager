80725211cea816b7e6f1a2ae38ffb8f79e2fe795
Patryk Stępnik <stepnikpa@gmail.com>
2024-09-13 22:33:40 +0200

    Swagger configuration

--- MealsManager.API/Program.cs
+++ MealsManager.API/Program.cs
@@ -5,7 +5,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Meals Manager API",
+        Version = "v1",
+        Description = "API for Cookbooks / Receipes management",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact() 
+        { 
+            Name = "Funigun", 
+            Url = new Uri(@"https://github.com/Funigun/MealsManager/tree/master") 
+        },
+    });
+    var filePath = Path.Combine(AppContext.BaseDirectory, "MealsManager.API.xml");
+    config.IncludeXmlComments(filePath);
+});
 
 var app = builder.Build();
 
@@ -13,7 +28,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MealManagerWebApp"));
 }
 
 app.UseHttpsRedirection();
