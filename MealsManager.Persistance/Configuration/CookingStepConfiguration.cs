34180d0eab4e89c5fa59019197284760416fb249
Patryk Stępnik <stepnikpa@gmail.com>
2024-10-09 19:40:06 +0200

    Added CookingStepConfiguration

---
+++ MealsManager.Persistance/Configuration/CookingStepConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using MealsManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealsManager.Persistance.Configuration
+{
+    internal class CookingStepConfiguration : IEntityTypeConfiguration<CookingStep>
+    {
+        public void Configure(EntityTypeBuilder<CookingStep> builder)
+        {
+            builder.Property(p => p.Type).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(1000);
+        }
+    }
+}
