3a64cb4ba05abf92944ac5459661af93812b351c
Patryk Stępnik <stepnikpa@gmail.com>
2024-10-09 20:07:09 +0200

    Added IngredientCategory configuration

---
+++ MealsManager.Persistance/Configuration/IngredientCategoryConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using MealsManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealsManager.Persistance.Configuration
+{
+    internal class IngredientCategoryConfiguration : IEntityTypeConfiguration<IngredientCategory>
+    {
+        public void Configure(EntityTypeBuilder<IngredientCategory> builder)
+        {
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
