e49a8a5c68544b885b39453c65a9add1fbef3d69
Patryk Stępnik <stepnikpa@gmail.com>
2024-10-16 00:05:00 +0200

    Created AuditableEntity base class

---
+++ MealsManager.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace MealsManager.Domain.Common
+{
+    public class AuditableEntity
+    {
+        public int Id { get; set; }
+
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+
+        public int StatusId { get; set; }
+        public string? InavtivatedBy { get; set; }
+        public DateTime? InactivatedOn { get; set; }
+    }
+}
